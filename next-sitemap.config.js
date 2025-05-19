d4f16f429dc4d8e887cfd1ea8071e4e773426fa0
Kinesh Lohar <kineshmalviya@gmail.com>
2025-05-19 14:08:34 +0530

    Add additional paths to sitemap configuration for improved SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,24 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://kineshlohar.vercel.app/',
-    generateRobotsTxt: true,
-    changefreq: 'weekly',
-    priority: 0.7,
-    sitemapSize: 5000,
-  };
+  siteUrl: 'https://kineshlohar.vercel.app/',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  additionalPaths: async (config) => {
+    return [
+      {
+        loc: '/projects',
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+      {
+        loc: '/educations-certifications',
+        changefreq: 'weekly',
+        priority: 0.7,
+        lastmod: new Date().toISOString(),
+      },
+    ]
+  },
+};
