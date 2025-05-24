b07ae6fbf4ef8146f1cba35e45131bf1d73fc7e5
Kinesh Lohar <kineshmalviya@gmail.com>
2025-05-24 20:07:50 +0530

    Add profile image and enhance AboutSection layout
    - Introduced a new profile image (kineshlohar.jpg) to the AboutSection component.
    - Updated the layout and styling of AboutSection for improved responsiveness and visual appeal.
    - Adjusted text alignment and spacing for better readability and presentation.

--- src/components/viewer/about/about-section.tsx
+++ src/components/viewer/about/about-section.tsx
@@ -1,18 +1,20 @@
+import Image from "next/image"
+
 
 
 export const AboutSection = () => {
     return (
-        <div className="w-full min-h-screen pt-32 sm:pt-24 pb-20 sm:pb-16 px-4 flex flex-col justify-between items-stretch ">
-            <h2 className="fade-up w-full sm:mt-6 text-left text-2xl sm:text-3xl md:text-4xl font-bold font-breeserif italic pr-12 sm:pr-0 sm:pl-8 md:pl-16 bg-gradient-to-b from-white to-zinc-300 bg-clip-text text-transparent transition-all duration-300">
-                A Bit About Me
-            </h2>
+        <div className="w-full min-h-screen pt-32 sm:pt-24 pb-8 md:pb-12 px-4 sm:px-8 md:px-16 flex flex-col md:flex-row items-stretch md:items-center justify-start md:justify-between gap-8">
+            <div className="w-full md:w-6/12 lg:w-7/12 flex flex-col justify-center items-start text-left gap-4 lg:pr-20">
+                <h2 className="fade-up w-full text-left text-2xl sm:text-3xl md:text-4xl font-bold font-breeserif italic pr-12 bg-gradient-to-b from-white to-zinc-300 bg-clip-text text-transparent transition-all duration-300">
+                    A Bit About Me
+                </h2>
 
-            <div className=' text-right text-sm md:text-base sm:w-8/12 lg:w-7/12 place-self-end sm:pr-16 font-lato'>
                 <p className="fade-up">
                     I&apos;m Kinesh Lohar, a Full-Stack Web Developer with a focus on building scalable, high-performance SaaS and complex software applications. I thrive on delivering clean, efficient code, prioritizing customer satisfaction, and am capable of working both independently and as part of a collaborative team.
                 </p>
 
-                <div className="flex flex-col sm:flex-row justify-center sm:justify-end items-end sm:items-center gap-3 sm:gap-6 mt-2">
+                <div className="flex justify-start items-center gap-6 md:mt-8">
                     <div className="fade-up flex flex-col items-center  justify-center border- border-white/70- px-8- sm:px-12- py-2 sm:py-3.5 ">
                         <div className="text-xl sm:text-2xl font-bold text-white/90">
                             2+
@@ -31,6 +33,49 @@ export const AboutSection = () => {
                     </div>
                 </div>
             </div>
+            <div className="fade-up relative w-full md:w-6/12 lg:w-5/12 h-[300px] min-[400px]:h-[400px] min-[450px]:h-[500px] md:h-[450px] transition-all duration-300 ">
+                <Image
+                src="/kineshlohar.jpg"
+                alt="Profile Picture of Kinesh Lohar"
+                fill
+                className=" object-cover object-right transition-all duration-300"
+                />
+            </div>
         </div >
     )
 }
+
+// export const AboutSection = () => {
+//     return (
+//         <div className="w-full min-h-screen pt-32 sm:pt-24 pb-20 sm:pb-16 px-4 flex flex-col justify-between items-stretch ">
+//             <h2 className=" w-full sm:mt-6 text-left text-2xl sm:text-3xl md:text-4xl font-bold font-breeserif italic pr-12 sm:pr-0 sm:pl-8 md:pl-16 bg-gradient-to-b from-white to-zinc-300 bg-clip-text text-transparent transition-all duration-300">
+//                 A Bit About Me
+//             </h2>
+
+//             <div className=' text-right text-sm md:text-base sm:w-8/12 lg:w-7/12 place-self-end sm:pr-16 font-lato'>
+//                 <p className="">
+//                     I&apos;m Kinesh Lohar, a Full-Stack Web Developer with a focus on building scalable, high-performance SaaS and complex software applications. I thrive on delivering clean, efficient code, prioritizing customer satisfaction, and am capable of working both independently and as part of a collaborative team.
+//                 </p>
+
+//                 <div className="flex flex-col sm:flex-row justify-center sm:justify-end items-end sm:items-center gap-3 sm:gap-6 mt-2">
+//                     <div className=" flex flex-col items-center  justify-center border- border-white/70- px-8- sm:px-12- py-2 sm:py-3.5 ">
+//                         <div className="text-xl sm:text-2xl font-bold text-white/90">
+//                             2+
+//                         </div>
+//                         <div className="text-xs opacity-85">
+//                             Years of Experience
+//                         </div>
+//                     </div>
+//                     <div className=" flex flex-col items-center  justify-center border- border-white/70- px-8- sm:px-12- py-2 sm:py-3.5 ">
+//                         <div className="text-xl sm:text-2xl font-bold text-white/90">
+//                             10+
+//                         </div>
+//                         <div className="text-xs opacity-85">
+//                             Projects Completed
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div >
+//     )
+// }
